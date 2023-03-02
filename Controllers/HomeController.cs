048f6565d2d4b30c9b93605e5bafbe44a3838468
Raygen Blackwell <raygen.m.black@gmail.com>
2023-03-01 21:11:03 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBooktoreRepository repo;
+        public HomeController (IBooktoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            
+            return View(x);
+        }
+        // Same thing, just a shortcut
+        // public IactionResult Index() => View();
+    }
+}
